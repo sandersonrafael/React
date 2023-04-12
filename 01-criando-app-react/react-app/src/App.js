da946e0dcd829ebdd06dbd1c2ea5d6a48fd4ec25
Sanderson Rafael <sandersonrafael-35@hotmail.com>
2023-04-11 23:31:52 -0300

    att

--- 01-criando-app-react/react-app/src/App.js
+++ 01-criando-app-react/react-app/src/App.js
@@ -1,7 +1,43 @@
-import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
 
-function App() {
+class App extends React.Component {
+    state = {
+        posts: [
+            {
+                id: 1,
+                title: "Título 1",
+                body: "Corpo 1",
+            },
+            {
+                id: 2,
+                title: "Título 2",
+                body: "Corpo 2",
+            },
+            {
+                id: 3,
+                title: "Título 3",
+                body: "Corpo 3",
+            },
+        ],
+    };
+
+    render() {
+        const { posts } = this.state;
+        return (
+            <div className="App">
+                {posts.map((post) => (
+                    <div key={post.id}>
+                        <h1>{post.title}</h1>
+                        <p>{post.body}</p>
+                    </div>
+                ))}
+            </div>
+        );
+    }
+}
+
+/* function App() {
     return (
         <div className="App">
             <header className="App-header">
@@ -18,6 +54,6 @@ function App() {
             </header>
         </div>
     );
-}
+} */
 
 export default App;
