913cd07cf04c2e2e608435c11e6a5f35a5ed51ea
Sanderson Rafael <sandersonrafael-35@hotmail.com>
2023-04-14 01:08:14 -0300

    useEffect

--- 02-react-hooks/app/src/App.js
+++ 02-react-hooks/app/src/App.js
@@ -1,43 +1,34 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
-import { useState } from 'react';
 
 export default function App() {
-    const [reverse, setReverse] = useState(false);
     const [counter, setCounter] = useState(0);
-    const reverseClass = reverse ? 'reverse' : '';
+    const [counter2, setCounter2] = useState(0);
 
-    const handleClick = () => {
-        setReverse(!reverse);
-    };
+    // // componentDidUpdate = () => {} -> executa sempre que atualizar os componentes
 
-    const handleCounter = () => {
-        setCounter(counter + 1);
-    };
+    // useEffect(() => {
+    //     console.log('componentDidUpdate');
+    // });
+
+    // // componentDidMount = () => {} -> executa 1x ao montar os componentes
+
+    // useEffect(() => {
+    //     console.log('componentDidMount');
+    // }, []);
+
+    // executa sempre que um determinado componente mudar (não todos) -> com dependência
+    useEffect(() => {
+        console.log('C1: ', counter, '; C2: ', counter2);
+    }, [counter, counter2]);
+
     return (
         <div className="App">
-            <header className="App-header">
-                <img
-                    src={logo}
-                    className={`App-logo ${reverseClass}`}
-                    alt="logo"
-                />
-
-                <h1>Contador: {counter}</h1>
-
-                <p>
-                    <button onClick={handleClick}>
-                        {reverse
-                            ? 'Mudar para sentido horário'
-                            : 'Mudar para sentido anti-horário'}
-                    </button>
-                </p>
-                <p>
-                    <button onClick={handleCounter}>
-                        Incrementar contador
-                    </button>
-                </p>
-            </header>
+            <h1>Contadores:</h1>
+            <h2>Contador 1: {counter}</h2>
+            <h2>Contador 2: {counter2}</h2>
+            <button onClick={() => setCounter(counter + 1)}>C1 + 1</button>{' '}
+            <button onClick={() => setCounter2(counter2 + 1)}>C2 + 1</button>
         </div>
     );
 }
