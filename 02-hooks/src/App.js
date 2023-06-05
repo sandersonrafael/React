ebedfd7494fe94d40cbaf3dbe32e0a074af35d59
Sanderson Rafael <sandersonrafael-35@hotmail.com>
2023-06-04 21:28:25 -0300

    Update App.js

--- 02-hooks/src/App.js
+++ 02-hooks/src/App.js
@@ -3,29 +3,11 @@ import './App.css';
 import { useState } from 'react';
 
 function App() {
-    const [reverse, setReverse] = useState(false);
     const [counter, setCounter] = useState(0);
-
-    const reverseClass = reverse ? 'reverse' : '';
-    const handleClick = () => {
-        setReverse(!reverse);
-        setCounter(counter + 1);
-    };
-
     return (
         <div className="App">
-            <header className="App-header">
-                <img
-                    src={logo}
-                    className={`App-logo ${reverseClass}`}
-                    alt="logo"
-                />
-
-                <h1>Contador: {counter}</h1>
-                <button type="button" onClick={handleClick}>
-                    Reverse {reverseClass}
-                </button>
-            </header>
+            <h1>Contador: {counter}</h1>
+            <button onClick={() => setCounter(counter + 1)}>+</button>
         </div>
     );
 }
