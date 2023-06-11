c8b59f54985db62554171040794702d6f1ce17f8
Sanderson Rafael <sandersonrafael-35@hotmail.com>
2023-06-11 00:17:32 -0300

    useContext pt2

---
+++ 02-hooks/src/components/Div/index.jsx
@@ -0,0 +1,12 @@
+import { H1 } from '../H1/index.jsx';
+import { P } from '../P/index.jsx';
+
+// eslint-disable-next-line
+export const Div = ({ children }) => {
+  return (
+    <>
+      <H1 />
+      <P />
+    </>
+  );
+};
