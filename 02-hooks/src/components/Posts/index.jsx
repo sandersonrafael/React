85a2e21d7fd897b1186ef45a6c857d2008a7ac40
Sanderson Rafael <sandersonrafael-35@hotmail.com>
2023-06-12 19:21:20 -0300

    novo hook organizado

--- 02-hooks/src/components/Posts/index.jsx
+++ 02-hooks/src/components/Posts/index.jsx
@@ -1,13 +1,19 @@
 import { useContext, useEffect, useRef } from 'react';
 import { PostsContext } from '../../contexts/PostsProvider/context';
 import { loadPosts } from '../../contexts/PostsProvider/actions';
+import { CounterContext } from '../../contexts/CounterProvider/context';
+import {
+  decrementCounter,
+  incrementCounter,
+} from '../../contexts/CounterProvider/action';
 
 export const Posts = () => {
   const isMounted = useRef(true);
   const postsContext = useContext(PostsContext);
   const { postsState, postsDispatch } = postsContext;
 
-  console.log(isMounted.current);
+  const counterContext = useContext(CounterContext);
+  const { counterState, counterDispatch } = counterContext;
 
   useEffect(() => {
     loadPosts(postsDispatch).then((dispatch) => {
@@ -23,7 +29,15 @@ export const Posts = () => {
 
   return (
     <div>
-      <h1>Opa</h1>
+      <button onClick={() => incrementCounter(counterDispatch)}>
+        Counter {counterState.counter} +
+      </button>
+
+      <button onClick={() => decrementCounter(counterDispatch)}>
+        Counter {counterState.counter} -
+      </button>
+
+      <h1>Posts</h1>
       {postsState.loading && <p>Carregando posts...</p>}
       {postsState.posts.map((posts) => (
         <p key={posts.id}>{posts.title}</p>
