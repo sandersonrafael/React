9c4db4c6201957af2f9568871e95687fef5a18b2
Sanderson Rafael <sandersonrafael-35@hotmail.com>
2023-06-12 13:23:23 -0300

    organização de projeto

---
+++ 02-hooks/src/components/Posts/index.jsx
@@ -0,0 +1,33 @@
+import { useContext, useEffect, useRef } from 'react';
+import { PostsContext } from '../../contexts/PostsProvider/context';
+import { loadPosts } from '../../contexts/PostsProvider/actions';
+
+export const Posts = () => {
+  const isMounted = useRef(true);
+  const postsContext = useContext(PostsContext);
+  const { postsState, postsDispatch } = postsContext;
+
+  console.log(isMounted.current);
+
+  useEffect(() => {
+    loadPosts(postsDispatch).then((dispatch) => {
+      if (isMounted.current) {
+        dispatch();
+      }
+    });
+
+    return () => {
+      isMounted.current = false;
+    };
+  }, [postsDispatch]);
+
+  return (
+    <div>
+      <h1>Opa</h1>
+      {postsState.loading && <p>Carregando posts...</p>}
+      {postsState.posts.map((posts) => (
+        <p key={posts.id}>{posts.title}</p>
+      ))}
+    </div>
+  );
+};
---
+++ 02-hooks/src/contexts/PostsProvider/index.jsx
@@ -0,0 +1,19 @@
+import PropTypes from 'prop-types';
+import { PostsContext } from './context';
+import { useReducer } from 'react';
+import { reducer } from './reducer';
+import { data } from './data';
+
+export const PostsProvider = ({ children }) => {
+  const [postsState, postsDispatch] = useReducer(reducer, data);
+
+  return (
+    <PostsContext.Provider value={{ postsState, postsDispatch }}>
+      {children}
+    </PostsContext.Provider>
+  );
+};
+
+PostsProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
